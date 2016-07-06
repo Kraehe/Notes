921e918cf4cc19a75d39e4f4b461d69065fe8108
Gia <gigi.giggles01@gmail.com>
2016-07-06 11:05:27 -0400

    moved 'before_action' from line 6 to line 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :authenticate_user!
   protect_from_forgery with: :exception
   # Forces a user that isn't logged in to login/sign up on all routes.
-  before_action :authenticate_user!
 end
